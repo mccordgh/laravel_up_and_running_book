d0a4788da41ab8b47a4c90054cf39e482e3c155d
Matthew McCord <mattm.eternal@gmail.com>
2017-09-01 15:59:31 -0500

    create userInfo table/migrations/seeding/Eloquent model

---
+++ app/UserInfo.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace LaravelUpAndRunning;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserInfo extends Model
+{
+    /**
+     * @var string
+     */
+    protected $dateFormat = 'Y-m-d H:i:s';
+
+    public function scopeStatus($query, $status)
+    {
+        return $query->where('status', $status)->OrderBy('last_name');
+    }
+//    public function scopeAllVips($query)
+//    {
+//        return $query->where('status', 'vip')->OrderBy('last_name');
+//    }
+//
+//    public function scopeAllMembers($query)
+//    {
+//        return $query->where('status', 'vip')->OrderBy('last_name');
+//    }
+//
+//    public function scopeAllNonMembers($query)
+//    {
+//        return $query->where('status', 'vip')->OrderBy('last_name');
+//    }
+//
+//    private function getStatus($status)
+//    {
+//        return $query->
+//    }
+}
---
+++ database/seeds/UserInfosTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserInfosTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->addUserInfo(['David', 'Sanchez', 'vip', 9001]);
+        $this->addUserInfo(['Joey', 'Salads', 'none', 0]);
+        $this->addUserInfo(['Amy', 'Pohler', 'vip', 2000]);
+        $this->addUserInfo(['Ronald', 'Grips', 'member', 200]);
+        $this->addUserInfo(['Amber', 'Stopid', 'none', 0]);
+        $this->addUserInfo(['Sally', 'Struthers', 'member', 500]);
+        $this->addUserInfo(['Bernie', 'Sanders', 'vip', 39999]);
+    }
+
+    private function addUserInfo($user) {
+        DB::table('user_infos')->insert([
+            'first_name' => $user[0],
+            'last_name' => $user[1],
+            'status' => $user[2],
+            'points' => $user[3],
+        ]);
+    }
+}
